edd529876e4b4add2dc0254038310850fd514aff
saltydog1980 <mikeelambert99@gmail.com>
2022-05-31 14:54:14 -0400

    semi-final code

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,22 @@
-class GuessingGame():
+import random
+class GuessingGame:
+    def __init__(self, answer):
+        self.answer = answer
+        self.correct = False
+        print(answer)
+
+    def guess(self, user_guess):
+        if user_guess > self.answer:
+            print('high')
+        elif user_guess < self.answer:
+            print('low')
+        else: 
+            user_guess == self.answer
+            print('correct')
+            self.correct = True
+    def solved(self):
+        print(self.correct)
+
+x = GuessingGame(random.randrange(10))
+x.guess(6)
+x.solved()
